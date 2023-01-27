21fed27863b9026c9b34abe700dfcb1e7186f4ec
Cristhian-shr <81269993+Cristhian-shr@users.noreply.github.com>
2023-01-27 11:02:22 -0300

    efeito digitação adicionado

--- js/script.js
+++ js/script.js
@@ -11,3 +11,30 @@ menuBar.addEventListener("click", function () {
 
   menu.classList.toggle("active");
 });
+
+// efeito digitação
+
+const typing = document.querySelector('[data-js="typing"]');
+const message = ["marketing de mídia social"];
+
+let messageIndex = 0;
+let characterIndex = 0;
+let currentMessage = "";
+let currentCharacters = "";
+
+const type = () => {
+  if (messageIndex === message.length) {
+    messageIndex = 0;
+  }
+
+  currentMessage = message[messageIndex];
+  currentCharacters = currentMessage.slice(0, characterIndex++);
+  typing.textContent = currentCharacters;
+
+  if (currentCharacters.length === currentMessage.length) {
+    messageIndex++;
+    characterIndex = 0;
+  }
+};
+
+setInterval(type, 200);
